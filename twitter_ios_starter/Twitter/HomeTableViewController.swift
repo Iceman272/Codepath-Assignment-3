74f5981e224e77d36d3c8de8b2305f8c6c90aa15
Iceman272 <nkdmack@yahoo.com>
2022-02-13 05:18:07 -0500

    Added Assignment 3 files to git
    Right before video 6

---
+++ twitter_ios_starter/Twitter/HomeTableViewController.swift
@@ -0,0 +1,39 @@
+//
+//  HomeTableViewController.swift
+//  Twitter
+//
+//  Created by Brandon Mack on 2/13/22.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewController: UITableViewController {
+
+    @IBAction func onLogout(_ sender: Any) {
+        TwitterAPICaller.client?.logout()
+        self.dismiss(animated: true, completion: nil)
+        UserDefaults.standard.set(false, forKey: "userLoggedIn")
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+
+}
